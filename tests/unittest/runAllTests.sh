ae13d5657e6ae96f8ff5db7384041eeffbeb9acf
Anja Gumpinger <anja.gumpinger@bsse.ethz.ch>
2020-02-17 17:36:38 +0100

    updated test file

--- tests/unittest/runAllTests.sh
+++ tests/unittest/runAllTests.sh
@@ -1,9 +1,9 @@
 # @Author: guanja
 # @Date:   2019-05-20 20:13:38
 # @Last Modified by:   Anja Gumpinger
-# @Last Modified time: 2020-02-17 17:06:58
+# @Last Modified time: 2020-02-17 17:36:28
 
-python3 -m unittest test_data.py
-python3 -m unittest test_features.py
-python3 -m unittest test_path_weights.py
 python3 -m unittest test_basegraph.py
+python3 -m unittest test_path_weights.py
+python3 -m unittest test_features.py
+python3 -m unittest test_data.py
